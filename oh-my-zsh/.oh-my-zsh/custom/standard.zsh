f6028fd65fd59391143254f8ec4cdc60278e1d71
De Pauw Stijn <smidpauw.depauw@ugent.be>
2020-02-19 12:37:16 +0100

    oh-my-zsh

---
+++ oh-my-zsh/.oh-my-zsh/custom/standard.zsh
@@ -0,0 +1,13 @@
+alias eb='vim ~/.zshrc'
+alias ev='vim ~/.vimrc'
+alias sb='source ~/.zshrc'
+alias cdzsh='cd ~/.oh-my-zsh/custom'
+alias c='clear'
+alias eal='vim ~/.oh-my-zsh/custom/standard.zsh'
+alias ei3='vim ~/.config/i3/config'
+alias ex='vim ~/.Xresources'
+alias sx='xrdb -load ~/.Xresources'
+alias kde='/usr/bin/kdeconnect-cli'
+alias eycm='vim ~/.vim/bundle/youcompleteme/.ycm_extra_conf.py'
+alias ra='. ranger' # Ranger to cd into the directory you quit in.
+alias dot='cd ~/dotfiles'
